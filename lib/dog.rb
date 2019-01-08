79a933a0ef27fe4c452253a71485176f13a6b6af
Nick Romans <nicholas.romans@gmail.com>
2019-01-08 18:54:39 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -80,8 +80,16 @@ class Dog
    end
 
    def self.find_by_id(id)
-     id_search = new_from_db(id)
-     binding.pry
+     sql = <<-SQL
+     SELECT *
+     FROM dogs
+     WHERE id = ?
+     LIMIT 1
+     SQL
+
+     DB[:conn].execute(sql, id).map do |row|
+       self.new_from_db(row)
+     end.first
    end
 
 end
